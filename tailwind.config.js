a1dfd006d4138ea1a815ff3e70aa1dc6122e197f
Tommy La <tommyl.dmd@gmail.com>
2021-12-29 12:47:35 -0800

    added work section and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  content: ["./homepage.html"],
+  content: ["./homepage.html", "./aboutme.html"],
   theme: {
     fontFamily: {
       'monster': ["'Montserrat'", 'sans-serif'],
+      'heebo': ["'Heebo", 'sans-serif'],
     },
       extend: {
         backgroundImage: (theme) => ({
           'homepage-main': "url('/Users/latommyla/My-Portfolio/imgs/homepage.jpeg')", 
-        })
+        }),
       },
     },
   plugins: [],
